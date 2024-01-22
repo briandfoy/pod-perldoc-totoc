b9f3c8889ec8c945d0d0574e2a08eb30af878c35
brian d foy <briandfoy@pobox.com>
2024-01-22 12:35:44 -0500

    Update email address

--- lib/Pod/TOC.pm
+++ lib/Pod/TOC.pm
@@ -126,11 +126,11 @@ This source is in Github:
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2006-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2006-2021, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
